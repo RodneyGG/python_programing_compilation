a3fc7c5b3efefc75768630974170d75c29fbeac7
RodneyGG <rodneyarevalo9963@gmail.com>
2025-03-10 21:25:18 +0800

    added a number validator

--- print_equal_if_num_same/print_equal_if_num_same.py
+++ print_equal_if_num_same/print_equal_if_num_same.py
@@ -1,8 +1,17 @@
 #Prog02: Create a program that ask user to input 2 numbers. Print "Equal" when the numbers are the same.
 
+#Check if number is valid
+def valid_num(msg):
+    while True: #Ask until the user input a valid number
+        num = input(msg).strip()
+        try:
+            return float(num)
+        except ValueError:
+            print("Invalid Input")
+
 #Input Two Numbers
-num1 = input("Enter Number 1: ")
-num2 = input("Enter Number 2: ")
+num1 = valid_num("Enter Number 1: ")
+num2 = valid_num("Enter Number 2: ")
 #Check if two numbers are equal
 if num1 == num2:
     #if equal print Equal
