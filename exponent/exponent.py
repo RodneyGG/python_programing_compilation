68394d09fb0fa5fbdb7f303887b9f60a28c21d47
RodneyGG <rodneyarevalo9963@gmail.com>
2025-03-10 23:17:27 +0800

    added a number validator

--- exponent/exponent.py
+++ exponent/exponent.py
@@ -1,9 +1,20 @@
 #Prog06: Create a program that ask user to input 2 numbers. Print the result when the first number is raised to the second number.
 
+#Check Valid Number
+def valid_num(msg):
+    while True:#ask until valid input
+        num = input(msg).strip()
+        try:
+            return float(num)
+        except ValueError:
+            print("Invalid Input")
+        
 #Input Two Numbers
-num1 = float(input("Enter Number: "))
-num2 = float(input("Enter Exponent: "))
+num1 = valid_num("Enter Number: ")
+num2 = valid_num("Enter Exponent: ")
+
 #Make the formula num1^num2
 answer = num1 ** num2
+
 #print answer
 print(answer)
