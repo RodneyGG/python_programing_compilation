0842f97847d8c71c68d2269f6a7d6a9f4678a09a
RodneyGG <rodneyarevalo9963@gmail.com>
2025-03-10 22:50:18 +0800

    print quotient

--- quotient_with_decimal_point/quotient_with_decimal_point.py
+++ quotient_with_decimal_point/quotient_with_decimal_point.py
@@ -1,4 +1,11 @@
 #Prog05: Create a program that ask user to input 2 numbers. Print the quotient of the two numbers with the decimal point
+
 #Input Two Numbers
+num1 = float(input("Enter Dividend: "))
+num2 = float(input("Enter Divisor:  "))
+
 #Divide Two Numbers
+quotient = num1 / num2
+
 #Print the quotient
+print(quotient)
