2de940c7c4bc38d48091f437ca769a724c182d73
RodneyGG <rodneyarevalo9963@gmail.com>
2025-03-10 20:55:38 +0800

    find the bigger number

--- find_bigger_number/find_bigger_num.py
+++ find_bigger_number/find_bigger_num.py
@@ -1,4 +1,12 @@
 #Prog01: Create a program that ask user to input 2 numbers. Print the bigger number.
+
 #Input Two Numbers
+num1 = input("Enter Number 1: ")
+num2 = input("Enter Number 2: ")
 #Find the bigger number
+if num1 > num2:
     #print the bigger number
+    print(num1)
+else: 
+    #print the bigger number
+    print(num2)
