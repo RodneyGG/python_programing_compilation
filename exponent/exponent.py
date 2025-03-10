06cd3b1c0002e381179db9c33b34ce1bd99cca39
RodneyGG <rodneyarevalo9963@gmail.com>
2025-03-10 23:14:01 +0800

    print answer

--- exponent/exponent.py
+++ exponent/exponent.py
@@ -1,4 +1,9 @@
 #Prog06: Create a program that ask user to input 2 numbers. Print the result when the first number is raised to the second number.
+
 #Input Two Numbers
+num1 = float(input("Enter Number: "))
+num2 = float(input("Enter Exponent: "))
 #Make the formula num1^num2
+answer = num1 ** num2
 #print answer
+print(answer)
