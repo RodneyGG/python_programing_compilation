0e19ea7c7a3a0f5d5d6828be354886d2bbdc782a
RodneyGG <rodneyarevalo9963@gmail.com>
2025-03-10 22:56:12 +0800

    added a number validator

--- quotient_with_decimal_point/quotient_with_decimal_point.py
+++ quotient_with_decimal_point/quotient_with_decimal_point.py
@@ -1,8 +1,17 @@
 #Prog05: Create a program that ask user to input 2 numbers. Print the quotient of the two numbers with the decimal point
 
+#Check if number is valid
+def valid_num(msg):
+    while True:#Ask until valid input
+        num = input(msg)
+        try:
+            return float(num)
+        except ValueError:
+            print("Invalid Input")
+
 #Input Two Numbers
-num1 = float(input("Enter Dividend: "))
-num2 = float(input("Enter Divisor:  "))
+num1 = valid_num("Enter Dividend: ")
+num2 = valid_num("Enter Divisor:  ")
 
 #Divide Two Numbers
 quotient = num1 / num2
