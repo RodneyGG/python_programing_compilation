6cbb5222480bf87b65100123f35756be391813dd
RodneyGG <rodneyarevalo9963@gmail.com>
2025-03-10 21:57:45 +0800

    print the product

--- product_two_num/product_two_num.py
+++ product_two_num/product_two_num.py
@@ -1,4 +1,9 @@
 #Prog04: Create a program that ask user to input 2 numbers. Print the product of the two numbers.
+
 #Input two number
+num1 = float(input("Enter Number 1: "))
+num2 = float(input("Enter Number 2: "))
 #Find the product
+product = num1 * num2 
 #print the product
+print(product)
