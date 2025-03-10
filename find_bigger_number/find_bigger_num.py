f81453e240df91dd916ecb6d31db68310f43d257
RodneyGG <rodneyarevalo9963@gmail.com>
2025-03-10 20:58:21 +0800

    added number validation

--- find_bigger_number/find_bigger_num.py
+++ find_bigger_number/find_bigger_num.py
@@ -1,8 +1,18 @@
 #Prog01: Create a program that ask user to input 2 numbers. Print the bigger number.
 
+#check if number is valid
+def valid_num(msg):
+    while True: #Ask until the user give a valid number
+        num = input(msg).strip()
+        try:
+            return float(num)
+        except ValueError:
+            print("Invalid Input")
+
 #Input Two Numbers
-num1 = input("Enter Number 1: ")
-num2 = input("Enter Number 2: ")
+num1 = valid_num("Enter Number 1: ")
+num2 = valid_num("Enter Number 2: ")
+
 #Find the bigger number
 if num1 > num2:
     #print the bigger number
