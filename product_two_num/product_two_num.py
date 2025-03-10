16b7f6eb8436f90693f21c4fb1597413167e337a
RodneyGG <rodneyarevalo9963@gmail.com>
2025-03-10 22:17:14 +0800

    add feature that ask user to quit

--- product_two_num/product_two_num.py
+++ product_two_num/product_two_num.py
@@ -9,10 +9,29 @@ def valid_num(msg):
         except ValueError:
             print("Invalid Input")
 
-#Input two number
-num1 = valid_num("Enter Number 1: ")
-num2 = valid_num("Enter Number 2: ")
-#Find the product
-product = num1 * num2
-#print the product
-print(product)
+#ask the user to quit            
+def ask_quit():
+    while True:#ask until valid input
+        ask_user = input("Do you wish to exit the program? (Y/N)").strip().lower()
+        #accept y/yes or n/no
+        if ask_user in ("y","yes"):
+            return False
+        elif ask_user in ("n","no"):
+            return True
+        else:
+            print("Invalid Input. Only Enter Y/N")
+
+try_again = True
+while try_again:
+    #Input two number
+    num1 = valid_num("Enter Number 1: ")
+    num2 = valid_num("Enter Number 2: ")
+    
+    #Find the product
+    product = num1 * num2
+    
+    #print the product
+    print(product)
+    
+    #Ask the user to quit
+    try_again = ask_quit()
