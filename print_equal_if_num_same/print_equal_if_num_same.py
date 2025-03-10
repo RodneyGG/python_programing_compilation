b8d61ce60ccf4e6e72eda3e3e8dd97f9c9ddcd9c
RodneyGG <rodneyarevalo9963@gmail.com>
2025-03-10 21:15:13 +0800

    print equal if same

--- print_equal_if_num_same/print_equal_if_num_same.py
+++ print_equal_if_num_same/print_equal_if_num_same.py
@@ -1,4 +1,9 @@
 #Prog02: Create a program that ask user to input 2 numbers. Print "Equal" when the numbers are the same.
+
 #Input Two Numbers
+num1 = input("Enter Number 1: ")
+num2 = input("Enter Number 2: ")
 #Check if two numbers are equal
+if num1 == num2:
     #if equal print Equal
+    print("Equal")
