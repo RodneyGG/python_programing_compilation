869ad4c3fa52d83731f7e15ccde4fbf0636e1114
RodneyGG <rodneyarevalo9963@gmail.com>
2025-03-10 23:10:21 +0800

    pseudocode

---
+++ exponent/exponent.py
@@ -0,0 +1,4 @@
+#Prog06: Create a program that ask user to input 2 numbers. Print the result when the first number is raised to the second number.
+#Input Two Numbers
+#Make the formula num1^num2
+#print answer
