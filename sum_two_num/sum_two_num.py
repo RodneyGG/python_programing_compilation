71b39eb3e7156f9d08f657187a5c9c2e8a1459b2
RodneyGG <rodneyarevalo9963@gmail.com>
2025-03-10 21:39:24 +0800

    print sum

--- sum_two_num/sum_two_num.py
+++ sum_two_num/sum_two_num.py
@@ -1,4 +1,9 @@
 #Prog03: Create a program that ask user to input 2 numbers. Print the sum of the two numbers.
+
 #Input Two numbers
+num1 = input("Enter Number 1")
+num2 = input("Enter Number 2")
 #Add the number
-#print the sum 
+sum = num1 + num2
+#print the sum
+print(sum) 
