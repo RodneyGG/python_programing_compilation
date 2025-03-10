1461a75ce7af676ab0006e98f86eeb75d2f33cc5
RodneyGG <rodneyarevalo9963@gmail.com>
2025-03-10 22:10:29 +0800

    added a number validation

--- product_two_num/product_two_num.py
+++ product_two_num/product_two_num.py
@@ -1,9 +1,18 @@
 #Prog04: Create a program that ask user to input 2 numbers. Print the product of the two numbers.
 
+#Check if valid number
+def valid_num(msg):
+    while True: #Ask Until user give a valid response
+        num = input(msg).strip()
+        try:
+            return float(num)
+        except ValueError:
+            print("Invalid Input")
+
 #Input two number
-num1 = float(input("Enter Number 1: "))
-num2 = float(input("Enter Number 2: "))
+num1 = valid_num("Enter Number 1: ")
+num2 = valid_num("Enter Number 2: ")
 #Find the product
-product = num1 * num2 
+product = num1 * num2
 #print the product
 print(product)
