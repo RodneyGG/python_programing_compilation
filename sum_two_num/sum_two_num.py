28b869aecbe14851b0e990f5c0dabf8dee761707
RodneyGG <rodneyarevalo9963@gmail.com>
2025-03-10 21:45:37 +0800

    added a feature that ask user to quit

--- sum_two_num/sum_two_num.py
+++ sum_two_num/sum_two_num.py
@@ -9,10 +9,27 @@ def valid_num(msg):
         except ValueError:
             print("Invalid Input")
 
-#Input Two numbers
-num1 = valid_num("Enter Number 1: ")
-num2 = valid_num("Enter Number 2: ")
-#Add the number
-sum = num1 + num2
-#print the sum
-print(sum) 
+#ask the user to quit
+def ask_quit():
+    while True:
+        ask_user = input("Do you want to quit the program? (Y/N)").lower().strip()
+        if ask_user in ("y","yes"):
+            return False
+        elif ask_user in ("n","no"):
+            return True
+        else:
+            print("Invalid Input. Only Enter Y/N")
+            
+try_again = True
+
+while try_again:
+    #Input Two numbers
+    num1 = valid_num("Enter Number 1: ")
+    num2 = valid_num("Enter Number 2: ")
+    #Add the number
+    sum = num1 + num2
+    #print the sum
+    print(sum) 
+    
+    #ask the user to quit 
+    try_again = ask_quit()
