d86051349ccd3e3b5c819fd15bf2ef3a9644262f
RodneyGG <rodneyarevalo9963@gmail.com>
2025-03-10 21:41:54 +0800

    added a number validator

--- sum_two_num/sum_two_num.py
+++ sum_two_num/sum_two_num.py
@@ -1,8 +1,17 @@
 #Prog03: Create a program that ask user to input 2 numbers. Print the sum of the two numbers.
 
+#Check if valid Number 
+def valid_num(msg):
+    while True: #Ask until user give a valid response 
+        num = input(msg).strip()
+        try:
+            return float(num)
+        except ValueError:
+            print("Invalid Input")
+
 #Input Two numbers
-num1 = input("Enter Number 1")
-num2 = input("Enter Number 2")
+num1 = valid_num("Enter Number 1: ")
+num2 = valid_num("Enter Number 2: ")
 #Add the number
 sum = num1 + num2
 #print the sum
