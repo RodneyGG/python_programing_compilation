76710dc29b2ba031f917b6a422cbd7b489e4f117
RodneyGG <rodneyarevalo9963@gmail.com>
2025-03-11 13:24:31 +0800

    added a number validator

--- sum_of_ten_numbers/sum_of_ten_numbers.py
+++ sum_of_ten_numbers/sum_of_ten_numbers.py
@@ -1,11 +1,20 @@
 #Prog07: Create a program that ask user to input 10 numbers. Print the sum of all the numbers.
 
+#Number Validator
+def valid_num(msg):
+    while True: #Ask until valid response
+        num = input(msg).strip()
+        try:
+            return float(num)
+        except ValueError:
+            print("Invalid Input")
+
 #Add a list to append the numbers
 numbers = []
 
 #Ask user to input Ten numbers
 for i in range(0,10):
-    num = float(input(f"Enter Number {i + 1}: "))
+    num = valid_num(f"Enter Number {i + 1}: ")
     numbers.append(num)
 
 #Add ten numbers and print the result
