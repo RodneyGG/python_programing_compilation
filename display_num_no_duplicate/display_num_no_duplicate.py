f622d1478fa00a6abf1ea82fd70fa4f027b1614c
RodneyGG <rodneyarevalo9963@gmail.com>
2025-03-11 23:26:35 +0800

    pseudocode

---
+++ display_num_no_duplicate/display_num_no_duplicate.py
@@ -0,0 +1,7 @@
+"""
+Prog02: Create a program that ask user to input 10 numbers. Display all numbers. 
+For numbers with duplicate, display only the first entry.
+"""
+#Ask user to input ten numbers
+#Add the number to the set
+#print the set
