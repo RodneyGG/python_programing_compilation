083f92d901ba98bcc3b1d698913b64bb6923e12e
RodneyGG <rodneyarevalo9963@gmail.com>
2025-03-11 20:11:41 +0800

    added a number validator

--- diff_two_num/diff_two_num.py
+++ diff_two_num/diff_two_num.py
@@ -1,8 +1,17 @@
 #Prog03: Create a program that ask user to input 2 numbers. Print the difference of the two numbers.
 
+#Add a number validator
+def valid_num(msg):
+    while True: #Ask until valid response
+        num = input(msg).strip()
+        try:
+            return float(num)
+        except ValueError:
+            print("Invalid Input")
+
 #Ask the user to input two numbers
-num1 = float(input("Enter Number: "))
-num2 = float(input("Enter Number: "))
+num1 = valid_num("Enter Number: ")
+num2 = valid_num("Enter Number: ")
 #find the diference of two numbers
 answer = num1 - num2
 #Print Answer
