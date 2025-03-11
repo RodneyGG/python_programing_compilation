637c3a0a2ab2323c5f8d55d3310452484d233f4b
RodneyGG <rodneyarevalo9963@gmail.com>
2025-03-11 15:24:29 +0800

    print only even

--- print_all_even_from_o_to_100/print_all_even_from_0_to_100.py
+++ print_all_even_from_o_to_100/print_all_even_from_0_to_100.py
@@ -2,6 +2,7 @@
 
 #Print 0 to 100
 for i in range(0,101):
-    print(i)
 #Check if the number is even
-    #print only even
+    if i % 2 == 0 and i != 0:
+        #print only even
+        print(i)
