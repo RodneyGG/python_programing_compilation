1797af7d1d171ada35144c5cd06ddec31c33b9d7
RodneyGG <rodneyarevalo9963@gmail.com>
2025-03-11 23:11:41 +0800

    find unique numbers and print it

--- display_unique_numbers/display_uniqeu_numbers.py
+++ display_unique_numbers/display_uniqeu_numbers.py
@@ -3,11 +3,18 @@ Prog01: Create a program that ask user to input 10 numbers. Display all numbers 
 """
 #List of numbers
 numbers = []
+unique_numbers = []
 
 #Ask the user to input 10 numbers
 for i in range(0,10):
-    num = float(input(f"Enter Number {i + 1}: "))
-    numbers.append(num)
+    number = float(input(f"Enter Number {i + 1}: "))
+    numbers.append(number)
+
 #Check the number that has no duplicate
+for num in numbers:
+    if numbers.count(num) == 1:
+        unique_numbers.append(num)
+
 #Print numbers that has no dubplicates
+print(unique_numbers)
 
