6909fe84bea33a6e1bd882be81a2db99eb938203
RodneyGG <rodneyarevalo9963@gmail.com>
2025-03-11 19:34:32 +0800

    print smaller number

--- print_smaller_num/print_smaller_num.py
+++ print_smaller_num/print_smaller_num.py
@@ -1,4 +1,14 @@
 #Prog01: Create a program that ask user to input 2 numbers. Print the smaller number.
+
 #Input Two numbers
+num1 = float(input("Enter Number 1: "))
+num2 = float(input("Enter Number 2: "))
+
 #Find the smaller number
+if num1 > num2:
     #Print The smaller number
+    print(num2)
+else:
+    #Print The smaller number
+    print(num1)
+    
