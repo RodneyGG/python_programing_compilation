fb46d7056d4b26b1231d34d7aead5a9cc0be13f2
RodneyGG <rodneyarevalo9963@gmail.com>
2025-03-11 21:28:38 +0800

    print 0 to 100 using while loop

--- print_odd_0_to_100_while_loop/print_odd_0_100_while_loop.py
+++ print_odd_0_to_100_while_loop/print_odd_0_100_while_loop.py
@@ -1,3 +1,7 @@
 #Prog08: Create a program that print all the odd numbers starting from 0 to 100. (Use while loop)
 #print 0 to 100 using while loop
+num = 0
+while num <= 100:
+    print(num)
+    num += 1
     #if number is odd print odd
