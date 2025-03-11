18ab75b7d7ddbe07cb55980b257a8b62a33b4abe
RodneyGG <rodneyarevalo9963@gmail.com>
2025-03-11 15:38:24 +0800

    pseudocode

---
+++ print_num_0_to_100_except_num_end_with_0/print_num_0_to_100_except_num_end_with_0.py
@@ -0,0 +1,4 @@
+#Prog10: Create a program that print all the numbers starting from 0 to 100 except numbers ending in zero.
+#print numbers from 0 to 100
+#if number does not end with 0
+    #print number
