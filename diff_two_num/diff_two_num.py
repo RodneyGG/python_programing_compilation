78d96355ec125d90360b2be3c94797001f4a4a79
RodneyGG <rodneyarevalo9963@gmail.com>
2025-03-11 20:05:38 +0800

    pseudocode

---
+++ diff_two_num/diff_two_num.py
@@ -0,0 +1,4 @@
+#Prog03: Create a program that ask user to input 2 numbers. Print the difference of the two numbers.
+#Ask the user to input two numbers
+#find the diference of two numbers
+#Print Answer
