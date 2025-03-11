9f72bdd84afdfbd52f78d6a2cd7a4b000e76dd58
RodneyGG <rodneyarevalo9963@gmail.com>
2025-03-11 22:19:31 +0800

    print num between two numbers

--- print_num_between_two_num/print_num_between_two_num.py
+++ print_num_between_two_num/print_num_between_two_num.py
@@ -1,4 +1,7 @@
 #Prog10: Create a program that ask user to input 2 numbers. Print all the numbers between the two numbers.
 #Ask the user to input two numbers
+num1 = int(input("Enter Number 1: "))
+num2 = int(input("Enter Number 2: "))
 #Print the numbers between the two numbers
-
+for num in range(num1 + 1, num2):
+    print(num)
