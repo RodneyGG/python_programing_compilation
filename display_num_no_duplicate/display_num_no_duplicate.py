fcbc8aa22522a37053df2ee99fa543ae708edc5b
RodneyGG <rodneyarevalo9963@gmail.com>
2025-03-11 23:29:28 +0800

    print numbers with no duplicate

--- display_num_no_duplicate/display_num_no_duplicate.py
+++ display_num_no_duplicate/display_num_no_duplicate.py
@@ -2,6 +2,14 @@
 Prog02: Create a program that ask user to input 10 numbers. Display all numbers. 
 For numbers with duplicate, display only the first entry.
 """
+
+#set
+numbers = set()
 #Ask user to input ten numbers
-#Add the number to the set
+for i in range(0,10):
+    num = float(input(f"Enter Number {i + 1}: "))
+    #Add the number to the set
+    numbers.add(num)
+
 #print the set
+print(numbers)
