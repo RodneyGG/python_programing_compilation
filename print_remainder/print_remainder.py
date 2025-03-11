9bddb56f156ffd0b27e7fb47e8c165117f5de374
RodneyGG <rodneyarevalo9963@gmail.com>
2025-03-11 21:57:23 +0800

    pseudocode

---
+++ print_remainder/print_remainder.py
@@ -0,0 +1,4 @@
+#Prog05: Create a program that ask user to input 2 numbers. Print the remainder when the first number is divided by the second number.
+#Enter the divident and divisor
+#Divide and find the remainder
+#print remainder
