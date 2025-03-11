6e78d65d52ed88d4905e85031db5ada9c8f4ee5e
RodneyGG <rodneyarevalo9963@gmail.com>
2025-03-11 19:50:51 +0800

    print not equal

--- print_not_equal_if_num_not_same/print_not_equal_if_num_not_same.py
+++ print_not_equal_if_num_not_same/print_not_equal_if_num_not_same.py
@@ -1,4 +1,10 @@
 #Prog02: Create a program that ask user to input 2 numbers. Print "Not Equal" when the numbers are not the same.
+
 #Input Two numbers
-#Check if num is the same 
+num1 = float(input("Enter Number 1: "))
+num2 = float(input("Enter Number 2: "))
+
+#Check if num is the not the same
+if num1 != num2: 
     #if number is not the same print "Not Equal"
+    print("Not Equal")
