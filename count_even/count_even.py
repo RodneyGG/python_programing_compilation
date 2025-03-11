96247a2f87f462242254de7e716d11e3d0db0c98
RodneyGG <rodneyarevalo9963@gmail.com>
2025-03-11 21:21:25 +0800

    Option to quit

--- count_even/count_even.py
+++ count_even/count_even.py
@@ -8,16 +8,34 @@ def valid_num(msg):
             return float(num)
         except ValueError:
             print("Invalid Input")
+            
+#ask user to quit
+def ask_quit():
+    while True: #Ask until valid response
+        ask_user = input("Do yo wish to exit the program? (Y/N)").lower().strip()
+        if ask_user in ("y", "yes"):
+            return False
+        elif ask_user in ("n", "no"):
+            return True
+        else:
+            print("Invalid Input")
 
-#add a counter
-count = 0
+try_again = True
 
-#Ask the user to input ten numbers
-for i in range(0,10):
-    num = valid_num(f"Enter Number {i + 1}")
-    #count even numbers
-    if num % 2 == 0 and num.is_integer():
-        count += 1
+while try_again: 
+    #add a counter
+    count = 0
 
-#print count of even
-print(count)
+    #Ask the user to input ten numbers
+    for i in range(0,10):
+        num = valid_num(f"Enter Number {i + 1}: ")
+        #count even numbers
+        if num % 2 == 0 and num.is_integer():
+            count += 1
+
+    #print count of even
+    print(count)
+    
+    #ask user to quit
+    try_again = ask_quit()
+    
