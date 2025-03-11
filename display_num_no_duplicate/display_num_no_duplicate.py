e22149848a1bb60ba718d7ccf8e4b1e7622619f5
RodneyGG <rodneyarevalo9963@gmail.com>
2025-03-11 23:32:17 +0800

    added an option to quit

--- display_num_no_duplicate/display_num_no_duplicate.py
+++ display_num_no_duplicate/display_num_no_duplicate.py
@@ -12,14 +12,29 @@ def valid_num(msg):
         except ValueError:
             print("Invalid Input")
 
-#set
-numbers = set()
+#ask user to quit
+def ask_quit():
+    while True:
+        ask_user = input("Do you wish to exit the program? (Y/N)").strip().lower()
+        if ask_user in ("y", "yes"):
+            return False
+        elif ask_user in ("n", "no"):
+            return True
+        else: 
+            print("Invalid Input")
 
-#Ask user to input ten numbers
-for i in range(0,10):
-    num = valid_num(f"Enter Number {i + 1}: ")
-    #Add the number to the set
-    numbers.add(num)
+try_again = True
+while try_again:
+    #set
+    numbers = set()
 
-#print the set
-print(numbers)
+    #Ask user to input ten numbers
+    for i in range(0,10):
+        num = valid_num(f"Enter Number {i + 1}: ")
+        #Add the number to the set
+        numbers.add(num)
+
+    #print the set
+    print(numbers)
+    
+    try_again = ask_quit()
