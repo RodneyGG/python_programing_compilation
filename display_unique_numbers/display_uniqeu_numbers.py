2cd04f59d6928ced70dbb066f3aac1fa82ef2b16
RodneyGG <rodneyarevalo9963@gmail.com>
2025-03-11 23:08:29 +0800

    put the 10 numbers on the list

--- display_unique_numbers/display_uniqeu_numbers.py
+++ display_unique_numbers/display_uniqeu_numbers.py
@@ -1,7 +1,13 @@
 """
 Prog01: Create a program that ask user to input 10 numbers. Display all numbers that don't have duplicate.
 """
+#List of numbers
+numbers = []
+
 #Ask the user to input 10 numbers
+for i in range(0,10):
+    num = float(input(f"Enter Number {i + 1}: "))
+    numbers.append(num)
 #Check the number that has no duplicate
 #Print numbers that has no dubplicates
 
