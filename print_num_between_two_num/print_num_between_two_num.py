42523c8c8fa11127ceadbee92bf949fdcccd7a7d
RodneyGG <rodneyarevalo9963@gmail.com>
2025-03-11 22:21:56 +0800

    fixed bug when num1 > num2

--- print_num_between_two_num/print_num_between_two_num.py
+++ print_num_between_two_num/print_num_between_two_num.py
@@ -2,6 +2,14 @@
 #Ask the user to input two numbers
 num1 = int(input("Enter Number 1: "))
 num2 = int(input("Enter Number 2: "))
+
+if num1 > num2:
+    bigger_num = num1
+    smaller_num = num2
+else:  
+    bigger_num = num2
+    smaller_num = num1
+    
 #Print the numbers between the two numbers
-for num in range(num1 + 1, num2):
+for num in range(smaller_num + 1, bigger_num):
     print(num)
