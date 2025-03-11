c8eb26dfc4074e0af8a179f4595bffbb3be0f558
RodneyGG <rodneyarevalo9963@gmail.com>
2025-03-11 15:44:04 +0800

    print num from 0 to 100 except num that end with 0

--- print_num_0_to_100_except_num_end_with_0/print_num_0_to_100_except_num_end_with_0.py
+++ print_num_0_to_100_except_num_end_with_0/print_num_0_to_100_except_num_end_with_0.py
@@ -2,6 +2,9 @@
 
 #print numbers from 0 to 100
 for i in range(0,101):
-    print(i)
-#if number does not end with 0
-    #print number
+    #if number does not end with 0
+    if i % 10 != 0 and i != 0:
+        #print number
+        print(i)
+
+    
