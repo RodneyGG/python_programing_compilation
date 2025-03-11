0cc63e2b41bd9367824e41940f64c4669b0d123f
RodneyGG <rodneyarevalo9963@gmail.com>
2025-03-11 21:01:29 +0800

    Added a number validator

--- diff_of_ten_num/diff_of_ten_num.py
+++ diff_of_ten_num/diff_of_ten_num.py
@@ -1,11 +1,20 @@
 #Prog06: Create a program that ask user to input 10 numbers. Print the result of the first number minus all of the remaining numbers.
 
+#Number Validator
+def valid_num(msg):
+    while True: #Ask Until Valid Response
+        num = input(msg).strip()
+        try:
+            return float(num)
+        except ValueError:
+            print("Invalid Input")
+
 #store the numbers in the list
 numbers = []
 
 #Ask the user to input ten numbers
 for i in range(0,10):
-    number = float(input(f"Input Number {i + 1}: "))
+    number = valid_num(f"Input Number {i + 1}: ")
     numbers.append(number)
     
 #Make the first number the intial result and subtract it with the next numbers
