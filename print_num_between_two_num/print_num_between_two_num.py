7117138168043ad75ddc14dd7817e61a4f0cd2fe
RodneyGG <rodneyarevalo9963@gmail.com>
2025-03-11 22:18:05 +0800

    pseudocode

---
+++ print_num_between_two_num/print_num_between_two_num.py
@@ -0,0 +1,4 @@
+#Prog10: Create a program that ask user to input 2 numbers. Print all the numbers between the two numbers.
+#Ask the user to input two numbers
+#Print the numbers between the two numbers
+
