47564315b4aba1c2d23252bcd3094a304d7f2de4
RodneyGG <rodneyarevalo9963@gmail.com>
2025-03-11 23:30:54 +0800

    added a number validator

--- display_num_no_duplicate/display_num_no_duplicate.py
+++ display_num_no_duplicate/display_num_no_duplicate.py
@@ -3,11 +3,21 @@ Prog02: Create a program that ask user to input 10 numbers. Display all numbers.
 For numbers with duplicate, display only the first entry.
 """
 
+#Validate Number
+def valid_num(msg):
+    while True:#ask until valid inpu
+        num = input(msg).strip()
+        try:
+            return float(num)
+        except ValueError:
+            print("Invalid Input")
+
 #set
 numbers = set()
+
 #Ask user to input ten numbers
 for i in range(0,10):
-    num = float(input(f"Enter Number {i + 1}: "))
+    num = valid_num(f"Enter Number {i + 1}: ")
     #Add the number to the set
     numbers.add(num)
 
