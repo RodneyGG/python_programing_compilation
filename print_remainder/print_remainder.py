12a5f740ea46b9569328b9040d8104ccdc2cd5f0
RodneyGG <rodneyarevalo9963@gmail.com>
2025-03-11 22:02:26 +0800

    add a number validator

--- print_remainder/print_remainder.py
+++ print_remainder/print_remainder.py
@@ -1,7 +1,17 @@
 #Prog05: Create a program that ask user to input 2 numbers. Print the remainder when the first number is divided by the second number.
+
+#Number Validator
+def valid_num(msg):
+    while True:
+        num = input(msg).strip()
+        try:
+            return float(num)
+        except ValueError:
+            print("Invalid Input")
+
 #Enter the divident and divisor
-num1 = float(input("Enter Divident: "))
-num2 = float(input("Enter Divisor:  "))
+num1 = valid_num("Enter Divident: ")
+num2 = valid_num("Enter Divisor:  ")
 #Divide and find the remainder
 remainder = num1 % num2
 #print remainder
