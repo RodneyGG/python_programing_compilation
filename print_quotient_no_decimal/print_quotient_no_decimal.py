5b05805890078c4969b41af726e9fd38b42b4755
RodneyGG <rodneyarevalo9963@gmail.com>
2025-03-11 20:22:45 +0800

    pseudocode

---
+++ print_quotient_no_decimal/print_quotient_no_decimal.py
@@ -0,0 +1,4 @@
+#Prog04: Create a program that ask user to input 2 numbers. Print the quotient of the two numbers without the decimal point
+#Ask the user to input two numbers
+#Find the quotient without the decimal(floor division)
+    #Print the quotient
