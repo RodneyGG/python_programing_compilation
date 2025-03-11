b228ac22edd9d7c03f9a5753d7980db00570bd97
RodneyGG <rodneyarevalo9963@gmail.com>
2025-03-11 19:43:31 +0800

    added a feature to quit

--- print_smaller_num/print_smaller_num.py
+++ print_smaller_num/print_smaller_num.py
@@ -2,23 +2,38 @@
 
 #Number Validator
 def valid_num(msg):
-    while True:
-        num = input().strip()
+    while True:#Ask until valid response
+        num = input(msg).strip()
         try:
             return float(num)
         except ValueError:
             print("Invalid Input")
 
+#ask the user to quit
+def ask_quit():
+    while True:#Ask until valid response
+        ask_user = input("Do you wish to exit the program? (Y/N)").lower().strip()
+        #accepts y/yes and n/no
+        if ask_user in ("y", "yes"):
+            return False
+        elif ask_user in ("n", "no"):
+            return True
+        else:
+            print("Invalid Input")
 
-#Input Two numbers
-num1 = valid_num("Enter Number 1: ")
-num2 = valid_num("Enter Number 2: ")
+try_again = True
+while try_again:
+    #Input Two numbers
+    num1 = valid_num("Enter Number 1: ")
+    num2 = valid_num("Enter Number 2: ")
 
-#Find the smaller number
-if num1 > num2:
-    #Print The smaller number
-    print(num2)
-else:
-    #Print The smaller number
-    print(num1)
+    #Find the smaller number
+    if num1 > num2:
+        #Print The smaller number
+        print(num2)
+    else:
+        #Print The smaller number
+        print(num1)
     
+    #ask user to quit
+    try_again = ask_quit()
