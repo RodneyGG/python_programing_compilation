37c903509211309c9ac238f38eb9457e8e8243b3
RodneyGG <rodneyarevalo9963@gmail.com>
2025-03-11 23:47:26 +0800

    handle errors

--- unique_or_duplicate/unique_or_duplicate.py
+++ unique_or_duplicate/unique_or_duplicate.py
@@ -1,15 +1,26 @@
 """
 Prog03: Create a program that ask user to input a number, continue asking until the user input is invalid. Display "Unique" after input when the inputted number don't have duplicate. Display "Duplicate" after input when the inputted number have duplicate.
 """
+
+def handle_error(msg):
+    while True:
+        num = input(msg)
+        try:
+            return float(num)
+        except ValueError:
+            return None
+
 #Make a empty list called numbers
 numbers = []
 
 #Prompt the user to enter a number until an error is occured
 while True:
-    num = float(input("Enter Number: "))
+    num = handle_error("Enter Number: ")
     
+    if num is None:
+        break
     #print unique or duplicate
-    if num in numbers:
+    elif num in numbers:
         print("Duplicate")
     else:
         print("Unique")
