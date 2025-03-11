61261f85d9e300762581cbc422aaed24d033bcef
RodneyGG <rodneyarevalo9963@gmail.com>
2025-03-11 22:07:05 +0800

    added a feature to quit

--- print_remainder/print_remainder.py
+++ print_remainder/print_remainder.py
@@ -2,17 +2,37 @@
 
 #Number Validator
 def valid_num(msg):
-    while True:
+    while True:#ask until valid response
         num = input(msg).strip()
         try:
             return float(num)
         except ValueError:
             print("Invalid Input")
 
-#Enter the divident and divisor
-num1 = valid_num("Enter Divident: ")
-num2 = valid_num("Enter Divisor:  ")
-#Divide and find the remainder
-remainder = num1 % num2
-#print remainder
-print(remainder)
+#ask user to quit            
+def ask_quit():
+    while True:#ask until valid input
+        ask_user = input("Do you wish to exit the program? (Y/N)").lower().strip()
+        #accept y/yes and n/no
+        if ask_user in ("y", "yes"):
+            return False
+        elif ask_user in ("n", "no"):
+            return True
+        else:
+            print("Invalid Input")
+                          
+
+try_again = True
+while try_again:
+    #Enter the divident and divisor
+    num1 = valid_num("Enter Divident: ")
+    num2 = valid_num("Enter Divisor:  ")
+    
+    #Divide and find the remainder
+    remainder = num1 % num2
+    
+    #print remainder
+    print(f"Remainder: {remainder}")
+    
+    #ask the user to quit 
+    try_again = ask_quit()
