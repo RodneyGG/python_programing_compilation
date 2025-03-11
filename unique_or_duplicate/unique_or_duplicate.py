ee36916a205c60b7b73caad2bc3853e684496056
RodneyGG <rodneyarevalo9963@gmail.com>
2025-03-11 23:38:26 +0800

    pseudocode

---
+++ unique_or_duplicate/unique_or_duplicate.py
@@ -0,0 +1,6 @@
+"""
+Prog03: Create a program that ask user to input a number, continue asking until the user input is invalid. Display "Unique" after input when the inputted number don't have duplicate. Display "Duplicate" after input when the inputted number have duplicate.
+"""
+#Make a empty list called numbers
+#Prompt the user to enter a number until an error is occured
+#print unique or duplicate
