aced1b8b0ff8afe52fe1d49bc96a7499d6f80e80
RodneyGG <rodneyarevalo9963@gmail.com>
2025-03-11 20:32:48 +0800

    added a number validation

--- print_quotient_no_decimal/print_quotient_no_decimal.py
+++ print_quotient_no_decimal/print_quotient_no_decimal.py
@@ -1,8 +1,17 @@
 #Prog04: Create a program that ask user to input 2 numbers. Print the quotient of the two numbers without the decimal point
 
+#Number Validator
+def valid_num(msg):
+    while True: #Ask until Valid Response
+        num = input(msg).strip()
+        try:
+            return float(num)
+        except ValueError:
+            print("Invalid Input")
+
 #Ask the user to input two numbers
-dividend = float(input("Enter Dividend: "))
-divisor = float(input("Enter Divisor: ")) 
+dividend = valid_num("Enter Dividend: ")
+divisor = valid_num("Enter Divisor: ")
 
 #Find the quotient without the decimal(floor division)
 quotient = int(dividend // divisor)
