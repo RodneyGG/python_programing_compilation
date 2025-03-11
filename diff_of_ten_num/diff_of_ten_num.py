26696cb5e86d181a6d20c461ad32df8fd2bef2e5
RodneyGG <rodneyarevalo9963@gmail.com>
2025-03-11 20:52:11 +0800

    Enter 10 numbers and put it in the list

--- diff_of_ten_num/diff_of_ten_num.py
+++ diff_of_ten_num/diff_of_ten_num.py
@@ -1,4 +1,12 @@
 #Prog06: Create a program that ask user to input 10 numbers. Print the result of the first number minus all of the remaining numbers.
+
+#store the numbers in the list
+numbers = []
+
 #Ask the user to input ten numbers
+for i in range(0,10):
+    num = float(input(f"Input Number {i + 1}: "))
+    numbers.append(num)
+    
 #Make the first number the intial result and subtract it with the next numbers
 #Print Answer
