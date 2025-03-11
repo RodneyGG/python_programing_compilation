6a0be921cae573480032211cdc71cd69018cd4ee
RodneyGG <rodneyarevalo9963@gmail.com>
2025-03-11 20:29:46 +0800

    print quotient w/out decimal

--- print_quotient_no_decimal/print_quotient_no_decimal.py
+++ print_quotient_no_decimal/print_quotient_no_decimal.py
@@ -1,4 +1,11 @@
 #Prog04: Create a program that ask user to input 2 numbers. Print the quotient of the two numbers without the decimal point
+
 #Ask the user to input two numbers
+dividend = float(input("Enter Dividend: "))
+divisor = float(input("Enter Divisor: ")) 
+
 #Find the quotient without the decimal(floor division)
-    #Print the quotient
+quotient = int(dividend // divisor)
+
+#Print the quotient
+print(quotient)
