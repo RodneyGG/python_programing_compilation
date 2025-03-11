8d1b96754b558dd4091d9cd7338f402dc7094f75
RodneyGG <rodneyarevalo9963@gmail.com>
2025-03-11 13:21:46 +0800

    sum of ten numbers

--- sum_of_ten_numbers/sum_of_ten_numbers.py
+++ sum_of_ten_numbers/sum_of_ten_numbers.py
@@ -1,4 +1,12 @@
 #Prog07: Create a program that ask user to input 10 numbers. Print the sum of all the numbers.
+
 #Add a list to append the numbers
+numbers = []
+
 #Ask user to input Ten numbers
+for i in range(0,10):
+    num = float(input(f"Enter Number {i + 1}: "))
+    numbers.append(num)
+
 #Add ten numbers and print the result
+print(sum(numbers))
