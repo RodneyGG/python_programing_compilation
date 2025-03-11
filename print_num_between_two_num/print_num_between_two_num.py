5f9162f9cf897cb626cdff2d539f4defc40a1749
RodneyGG <rodneyarevalo9963@gmail.com>
2025-03-11 22:43:11 +0800

    added an option to quit

--- print_num_between_two_num/print_num_between_two_num.py
+++ print_num_between_two_num/print_num_between_two_num.py
@@ -12,18 +12,34 @@ def valid_num(msg):
             return int(num)
         except ValueError:
             print("Invalid Input")
-        
-#Ask the user to input two numbers
-num1 = valid_num("Enter Number 1: ")
-num2 = valid_num("Enter Number 2: ")
+            
+#ask user to quit
+def ask_quit():
+    while True:#ask until valid input
+        ask_user = input("Do you wish to exit the program? (Y/N)").lower().strip()
+        #accept y/yes and n/no
+        if ask_user in ("y", "yes"):
+            return False
+        elif ask_user in ("n", "no"):
+            return True
+        else:
+            print("Invalid Input")
 
-if num1 > num2:
-    bigger_num = num1
-    smaller_num = num2
-else:  
-    bigger_num = num2
-    smaller_num = num1
+try_again = True
+while try_again:       
+    #Ask the user to input two numbers
+    num1 = valid_num("Enter Number 1: ")
+    num2 = valid_num("Enter Number 2: ")
+
+    if num1 > num2:
+        bigger_num = num1
+        smaller_num = num2
+    else:  
+        bigger_num = num2
+        smaller_num = num1
+        
+    #Print the numbers between the two numbers
+    for num in range(smaller_num + 1, bigger_num):
+        print(num)
     
-#Print the numbers between the two numbers
-for num in range(smaller_num + 1, bigger_num):
-    print(num)
+    try_again = ask_quit()
