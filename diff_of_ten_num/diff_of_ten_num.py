4d2d8c0013736c83a1f574b6fbe205d06e73943c
RodneyGG <rodneyarevalo9963@gmail.com>
2025-03-11 20:49:19 +0800

    pseudocode

---
+++ diff_of_ten_num/diff_of_ten_num.py
@@ -0,0 +1,4 @@
+#Prog06: Create a program that ask user to input 10 numbers. Print the result of the first number minus all of the remaining numbers.
+#Ask the user to input ten numbers
+#Make the first number the intial result and subtract it with the next numbers
+#Print Answer
