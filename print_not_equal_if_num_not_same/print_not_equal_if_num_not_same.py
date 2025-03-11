573cb75c2848ae2049509d391d24cc583bc42e8a
RodneyGG <rodneyarevalo9963@gmail.com>
2025-03-11 19:48:09 +0800

    pseudocode

---
+++ print_not_equal_if_num_not_same/print_not_equal_if_num_not_same.py
@@ -0,0 +1,4 @@
+#Prog02: Create a program that ask user to input 2 numbers. Print "Not Equal" when the numbers are not the same.
+#Input Two numbers
+#Check if num is the same 
+    #if number is not the same print "Not Equal"
