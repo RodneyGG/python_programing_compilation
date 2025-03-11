42bd9a29c9a0458c1ea5ccd613057391c90dca88
RodneyGG <rodneyarevalo9963@gmail.com>
2025-03-11 21:59:32 +0800

    print remainder

--- print_remainder/print_remainder.py
+++ print_remainder/print_remainder.py
@@ -1,4 +1,8 @@
 #Prog05: Create a program that ask user to input 2 numbers. Print the remainder when the first number is divided by the second number.
 #Enter the divident and divisor
+num1 = float(input("Enter Divident: "))
+num2 = float(input("Enter Divisor:  "))
 #Divide and find the remainder
+remainder = num1 % num2
 #print remainder
+print(remainder)
