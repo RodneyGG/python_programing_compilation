766f078807eee353a5f327484a4215e994d400aa
RodneyGG <rodneyarevalo9963@gmail.com>
2025-03-11 23:06:06 +0800

    pseudocode

---
+++ display_unique_numbers/display_uniqeu_numbers.py
@@ -0,0 +1,7 @@
+"""
+Prog01: Create a program that ask user to input 10 numbers. Display all numbers that don't have duplicate.
+"""
+#Ask the user to input 10 numbers
+#Check the number that has no duplicate
+#Print numbers that has no dubplicates
+
