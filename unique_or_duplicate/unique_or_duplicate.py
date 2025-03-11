a30fc380f9ef5482d7def7d2db7a3bb3b9a2410e
RodneyGG <rodneyarevalo9963@gmail.com>
2025-03-11 23:42:58 +0800

    display unique or duplicate

--- unique_or_duplicate/unique_or_duplicate.py
+++ unique_or_duplicate/unique_or_duplicate.py
@@ -2,5 +2,16 @@
 Prog03: Create a program that ask user to input a number, continue asking until the user input is invalid. Display "Unique" after input when the inputted number don't have duplicate. Display "Duplicate" after input when the inputted number have duplicate.
 """
 #Make a empty list called numbers
+numbers = []
+
 #Prompt the user to enter a number until an error is occured
-#print unique or duplicate
+while True:
+    num = float(input("Enter Number: "))
+    
+    #print unique or duplicate
+    if num in numbers:
+        print("Duplicate")
+    else:
+        print("Unique")
+        numbers.append(num)
+
