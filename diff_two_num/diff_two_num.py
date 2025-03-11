c46dbc4d4066d67265ab3128309ccfea24b04791
RodneyGG <rodneyarevalo9963@gmail.com>
2025-03-11 20:19:08 +0800

    added an option to quit

--- diff_two_num/diff_two_num.py
+++ diff_two_num/diff_two_num.py
@@ -9,10 +9,29 @@ def valid_num(msg):
         except ValueError:
             print("Invalid Input")
 
-#Ask the user to input two numbers
-num1 = valid_num("Enter Number: ")
-num2 = valid_num("Enter Number: ")
-#find the diference of two numbers
-answer = num1 - num2
-#Print Answer
-print(f"{num1} - {num2} = {answer}")
+#ask user to quit
+def ask_quit():
+    while True:#Ask Until Valid Response
+        ask_user = input("Do you wish to exit the program? (Y/N)").lower().strip()
+        #accept y/yes and n/no
+        if ask_user in ("y", "yes"):
+            return False
+        elif ask_user in ("n", "no"):
+            return True
+        else:
+            print("Invalid Input")
+
+try_again = True
+while try_again:
+    #Ask the user to input two numbers
+    num1 = valid_num("Enter Number: ")
+    num2 = valid_num("Enter Number: ")
+    
+    #find the diference of two numbers
+    answer = num1 - num2
+    
+    #Print Answer
+    print(f"{num1} - {num2} = {answer}")
+    
+    #ask user to quit
+    try_again = ask_quit()
