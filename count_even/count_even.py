82eb09357701faa2fca5680caee2f08b4d17e473
RodneyGG <rodneyarevalo9963@gmail.com>
2025-03-11 21:16:46 +0800

    add a number validator

--- count_even/count_even.py
+++ count_even/count_even.py
@@ -1,11 +1,20 @@
 #Prog07: Create a program that ask user to input 10 numbers. Print how many are even numbers.
 
+#Number Validator
+def valid_num(msg):
+    while True: #Ask Until Valid Response
+        num = input(msg).strip()
+        try:
+            return float(num)
+        except ValueError:
+            print("Invalid Input")
+
 #add a counter
 count = 0
 
 #Ask the user to input ten numbers
 for i in range(0,10):
-    num = float(input(f"Enter Number {i + 1}"))
+    num = valid_num(f"Enter Number {i + 1}")
     #count even numbers
     if num % 2 == 0 and num.is_integer():
         count += 1
