f69d265b75efdb05662a9360ba70766c5f2bc016
RodneyGG <rodneyarevalo9963@gmail.com>
2025-03-11 13:11:23 +0800

    pseudocode

---
+++ sum_of_ten_numbers/sum_of_ten_numbers.py
@@ -0,0 +1,4 @@
+#Prog07: Create a program that ask user to input 10 numbers. Print the sum of all the numbers.
+#Add a list to append the numbers
+#Ask user to input Ten numbers
+#Add ten numbers and print the result
