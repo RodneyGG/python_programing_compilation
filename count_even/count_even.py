1434f65029420f99d0cbc3eb886d75e3725bf28e
RodneyGG <rodneyarevalo9963@gmail.com>
2025-03-11 21:15:38 +0800

    count even num

--- count_even/count_even.py
+++ count_even/count_even.py
@@ -1,4 +1,14 @@
 #Prog07: Create a program that ask user to input 10 numbers. Print how many are even numbers.
+
+#add a counter
+count = 0
+
 #Ask the user to input ten numbers
-#count even numbers
+for i in range(0,10):
+    num = float(input(f"Enter Number {i + 1}"))
+    #count even numbers
+    if num % 2 == 0 and num.is_integer():
+        count += 1
+
 #print count of even
+print(count)
