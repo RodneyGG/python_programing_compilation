30006a7b69238cfbeed64aac3cd2fec8795363fd
RodneyGG <rodneyarevalo9963@gmail.com>
2025-03-11 22:35:33 +0800

    Number Validator. Won't accept float

--- print_num_between_two_num/print_num_between_two_num.py
+++ print_num_between_two_num/print_num_between_two_num.py
@@ -1,7 +1,21 @@
 #Prog10: Create a program that ask user to input 2 numbers. Print all the numbers between the two numbers.
+
+#Number Validator
+def valid_num(msg):
+    while True:
+        num = input(msg).strip()
+        try:
+            num = float(num)
+            if num != int(num):
+                print("This program does not accept numbers with decimal")
+                continue
+            return int(num)
+        except ValueError:
+            print("Invalid Input")
+        
 #Ask the user to input two numbers
-num1 = int(input("Enter Number 1: "))
-num2 = int(input("Enter Number 2: "))
+num1 = valid_num("Enter Number 1: ")
+num2 = valid_num("Enter Number 2: ")
 
 if num1 > num2:
     bigger_num = num1
