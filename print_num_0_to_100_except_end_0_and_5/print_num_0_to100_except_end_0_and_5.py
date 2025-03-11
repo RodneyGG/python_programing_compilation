4aef1d294c0e339c32db12697d22197243af9133
RodneyGG <rodneyarevalo9963@gmail.com>
2025-03-11 21:44:21 +0800

    print 0 to 100

--- print_num_0_to_100_except_end_0_and_5/print_num_0_to100_except_end_0_and_5.py
+++ print_num_0_to_100_except_end_0_and_5/print_num_0_to100_except_end_0_and_5.py
@@ -1,4 +1,6 @@
 #Prog09: Create a program that print all the numbers starting from 0 to 100 except numbers ending in zero or ending five.
 #Print numbers from 0 to 100
+for i in range(0,101):
+    print(i)
 #Check if number end with 0 and 5
     #if not print the number
