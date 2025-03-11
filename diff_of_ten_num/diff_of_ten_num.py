e0b9ad8a577bf3e37a6967a68e492c7653fe9b2b
RodneyGG <rodneyarevalo9963@gmail.com>
2025-03-11 20:57:22 +0800

    print the result

--- diff_of_ten_num/diff_of_ten_num.py
+++ diff_of_ten_num/diff_of_ten_num.py
@@ -5,8 +5,12 @@ numbers = []
 
 #Ask the user to input ten numbers
 for i in range(0,10):
-    num = float(input(f"Input Number {i + 1}: "))
-    numbers.append(num)
+    number = float(input(f"Input Number {i + 1}: "))
+    numbers.append(number)
     
 #Make the first number the intial result and subtract it with the next numbers
+result = numbers[0]
+for num in numbers[1:]:
+    result -= num 
 #Print Answer
+print(result)
