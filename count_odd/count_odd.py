51b33b73d631b9ac9b0eda6a2ee9f9912cf05ef4
RodneyGG <rodneyarevalo9963@gmail.com>
2025-03-11 13:51:36 +0800

    pseudocode

---
+++ count_odd/count_odd.py
@@ -0,0 +1,4 @@
+#Prog08: Create a program that ask user to input 10 numbers. Print how many are odd numbers.
+#Ask the user to input ten numbers
+#count the odd numbers
+#print the number of odd count
