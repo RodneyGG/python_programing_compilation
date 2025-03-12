2062a093acf87826959f175a2d20dd1968bafc11
RodneyGG <rodneyarevalo9963@gmail.com>
2025-03-12 09:58:08 +0800

    pseudocode

---
+++ print_duplicate_num/print_duplicate_num.py
@@ -0,0 +1,8 @@
+"""
+Prog01: Create a program that ask user to input 10 numbers. Display all numbers that have duplicate.
+"""
+#Make a list and set
+#ask user to print ten numbers
+#Append all numbers in the list
+    #if number already in the list add to the set
+    #print the set
