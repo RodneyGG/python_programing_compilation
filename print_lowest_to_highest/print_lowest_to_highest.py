f1f44c6bf2e8f5c87797a05bc45f4b4e2a90ad1f
RodneyGG <rodneyarevalo9963@gmail.com>
2025-03-12 09:51:53 +0800

    print in lowest to highest order. Fixed breaking

--- print_lowest_to_highest/print_lowest_to_highest.py
+++ print_lowest_to_highest/print_lowest_to_highest.py
@@ -2,8 +2,29 @@
 Prog05: Create a program that ask user to input a number, continue asking until the user input is invalid. 
 Display the number from lowest to highest. Clue: sort() function
 """
+#handle errors
+def handle_error(msg):
+    num = input(msg).strip()
+    try: 
+        return float(num)
+    except ValueError:
+        return None
+
 #make a list called numbers
+numbers = []
+
 #Ask the user to input a number until error has occured
-#Append number to the list
-#sort the numbers
-#print the sorted numbers
+while True:
+    num = handle_error("Enter Number: ")
+    
+    if num is None:
+        break
+    
+    #Append number to the list
+    numbers.append(num)
+
+if numbers:
+    #sort the numbers
+    numbers.sort()
+    #print the sorted numbers
+    print(numbers)
