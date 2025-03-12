f4c5aeae1e079ad638658bf1cb8f4d0b0b10cca8
RodneyGG <rodneyarevalo9963@gmail.com>
2025-03-12 09:18:49 +0800

    print the lowest num but it cant break

--- print_lowest_num/print_lowest_num.py
+++ print_lowest_num/print_lowest_num.py
@@ -1,7 +1,34 @@
 """
 Prog04: Create a program that ask user to input a number, continue asking until the user input is invalid. Display the lowest number
 """
+
+def handle_error(msg):
+    while True:
+        num = input(msg)
+        try:
+            return float(num)
+        except ValueError:
+            if numbers:
+                print(lowest_num)
+            else:
+                break
+
 #Make a list to store the numbers
+numbers = []
+
+
+
 #Ask user to input a number until error has occured
-#append numbers to the list 
-#Print the lowest number
+while True:
+    num = handle_error("Enter a Number: ")
+    #append numbers to the list 
+    numbers.append(num)
+    #identify which is the smaller number
+    lowest_num = min(numbers)
+    
+    
+    
+   
+
+
+
