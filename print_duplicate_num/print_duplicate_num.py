95250554f541130ab900d220b54f260e56f844d0
RodneyGG <rodneyarevalo9963@gmail.com>
2025-03-12 10:05:01 +0800

    added a number validator

--- print_duplicate_num/print_duplicate_num.py
+++ print_duplicate_num/print_duplicate_num.py
@@ -1,13 +1,23 @@
 """
 Prog01: Create a program that ask user to input 10 numbers. Display all numbers that have duplicate.
 """
+#Number Validator
+def valid_num(msg):
+    while True: #ask until valid response
+        num = input(msg).strip()
+        try:
+            return float(num)
+        except ValueError:
+            print("Invalid Input")
+
+
 #Make a list and set
 numbers = []
 duplicate_numbers = set()
 
 #ask user to print ten numbers
 for i in range(0,10):
-    num = float(input(f"Enter a number {i + 1}: "))
+    num = valid_num(f"Enter a number {i + 1}: ")
     #Append all numbers in the list
     if num in numbers:
     #if number already in the list add to the set
