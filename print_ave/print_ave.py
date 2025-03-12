5d67c34d261556d0d26010f78a92139c965bd4f6
RodneyGG <rodneyarevalo9963@gmail.com>
2025-03-12 11:19:42 +0800

    handle error and print ave

--- print_ave/print_ave.py
+++ print_ave/print_ave.py
@@ -4,16 +4,30 @@ continue asking until the user input is invalid.
 Display the average.
 """
 
+#Handle Error
+def handle_error(msg):
+    num = input(msg).strip()
+    try:
+        return float(num)
+    except ValueError:
+        return None
+
 #Variables
 total = 0
 count = 0
 
 #Ask the user to input number, it will break once the user 
 while True:
-    num = float(input("Enter Number"))
+    num = handle_error("Enter Number: ")
+    
+    if num is None:
+        break
+    
     total += num
     count += 1
-    
+
+
+if count > 0:
     #Take the total value and divide it with the total number of entry
     average = total / count
     #display average
