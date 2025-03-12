f95597e843b00833d1e108be74ea9ad548dfb7ac
RodneyGG <rodneyarevalo9963@gmail.com>
2025-03-12 11:13:48 +0800

    pseudocode

---
+++ print_ave/print_ave.py
@@ -0,0 +1,8 @@
+"""
+Prog05: Create a program that ask user to input a number, 
+continue asking until the user input is invalid. 
+Display the average.
+"""
+#Ask the user to input number, it will break once the user 
+#Take the total value and divide it with the total number of entry
+#display average
