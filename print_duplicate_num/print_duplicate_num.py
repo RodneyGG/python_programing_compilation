e9faae7e674cb6be677bd8bcb112133187ce23b9
RodneyGG <rodneyarevalo9963@gmail.com>
2025-03-12 10:02:05 +0800

    print duplicate numbers

--- print_duplicate_num/print_duplicate_num.py
+++ print_duplicate_num/print_duplicate_num.py
@@ -2,7 +2,18 @@
 Prog01: Create a program that ask user to input 10 numbers. Display all numbers that have duplicate.
 """
 #Make a list and set
+numbers = []
+duplicate_numbers = set()
+
 #ask user to print ten numbers
-#Append all numbers in the list
+for i in range(0,10):
+    num = float(input(f"Enter a number {i + 1}: "))
+    #Append all numbers in the list
+    if num in numbers:
     #if number already in the list add to the set
-    #print the set
+        duplicate_numbers.add(num)
+    #add every number to the list
+    numbers.append(num)
+    
+#print the set
+print(duplicate_numbers)
