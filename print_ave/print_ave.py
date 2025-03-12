0e1dc3bfd42527a00e2654efa2bd719016c84656
RodneyGG <rodneyarevalo9963@gmail.com>
2025-03-12 11:16:10 +0800

    store the values in total and count

--- print_ave/print_ave.py
+++ print_ave/print_ave.py
@@ -3,6 +3,18 @@ Prog05: Create a program that ask user to input a number,
 continue asking until the user input is invalid. 
 Display the average.
 """
+
+#Variables
+total = 0
+count = 0
+
 #Ask the user to input number, it will break once the user 
-#Take the total value and divide it with the total number of entry
-#display average
+while True:
+    num = float(input("Enter Number"))
+    total += num
+    count += 1
+    
+    #Take the total value and divide it with the total number of entry
+    average = total / count
+    #display average
+    print(average)
