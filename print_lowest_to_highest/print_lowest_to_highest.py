06ef50d8296035cc1ae872126fb384c33bdf54e5
RodneyGG <rodneyarevalo9963@gmail.com>
2025-03-12 09:31:55 +0800

    pseudocode

---
+++ print_lowest_to_highest/print_lowest_to_highest.py
@@ -0,0 +1,9 @@
+"""
+Prog05: Create a program that ask user to input a number, continue asking until the user input is invalid. 
+Display the number from lowest to highest. Clue: sort() function
+"""
+#make a list called numbers
+#Ask the user to input a number until error has occured
+#Append number to the list
+#sort the numbers
+#print the sorted numbers
