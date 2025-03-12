ac293abe085fdd60ac266adc44df3fa98352d07f
RodneyGG <rodneyarevalo9963@gmail.com>
2025-03-12 10:20:44 +0800

    pseudocode

---
+++ print_num_with_most_duplicate/print_num_with_most_duplicate.py
@@ -0,0 +1,8 @@
+"""
+Prog02: Create a program that ask user to input a number, continue asking until the user input is invalid. 
+Display the number with the most number of duplicate.
+"""
+#make a dictionary
+#Ask user to input a number until an error has occured
+#Find the number with the most duplicate
+    #print the number
