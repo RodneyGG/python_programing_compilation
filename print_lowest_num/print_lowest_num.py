349d845671ce967bc77de58475d8d68f6921f755
RodneyGG <rodneyarevalo9963@gmail.com>
2025-03-12 09:03:00 +0800

    pseudocode

---
+++ print_lowest_num/print_lowest_num.py
@@ -0,0 +1,7 @@
+"""
+Prog04: Create a program that ask user to input a number, continue asking until the user input is invalid. Display the lowest number
+"""
+#Make a list to store the numbers
+#Ask user to input a number until error has occured
+#append numbers to the list 
+#Print the lowest number
