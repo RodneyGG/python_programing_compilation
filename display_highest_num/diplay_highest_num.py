e7e32b50793c748e8009aa9b7b5d86c7a591090b
RodneyGG <rodneyarevalo9963@gmail.com>
2025-03-12 10:50:37 +0800

    print the highes num and handle error

--- display_highest_num/diplay_highest_num.py
+++ display_highest_num/diplay_highest_num.py
@@ -3,7 +3,28 @@ Prog03: Create a program that ask user to input a number,
 continue asking until the user input is invalid. 
 Display the highest number
 """
+#Handle error
+def handle_error(msg):
+    num = input(msg)
+    try:
+        return float(num)
+    except ValueError:
+        return None
+
 #make a list called numbers
+numbers = []
 #ask user to input number until error has occured
-#find the highest number 
+while True:
+    num = handle_error("Enter Number: ")
+    
+    if num is None:
+        break
+    
+    numbers.append(num)
+
+ 
+if numbers:
+    #find the highest number
+    highest_num = max(numbers)
     #print the number
+    print(highest_num)
