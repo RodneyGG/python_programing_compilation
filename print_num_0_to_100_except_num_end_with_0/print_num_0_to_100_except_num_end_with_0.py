d5bd48b285224ac6ed255f823d1aa112d47876ab
RodneyGG <rodneyarevalo9963@gmail.com>
2025-03-13 08:08:03 +0800

    fixed spaces after comma

--- print_num_0_to_100_except_num_end_with_0/print_num_0_to_100_except_num_end_with_0.py
+++ print_num_0_to_100_except_num_end_with_0/print_num_0_to_100_except_num_end_with_0.py
@@ -15,7 +15,7 @@ try_again = True
 
 while try_again:
     #print numbers from 0 to 100
-    for i in range(0,101):
+    for i in range(0, 101):
         #if number does not end with 0
         if i % 10 != 0 and i != 0:
             #print number
