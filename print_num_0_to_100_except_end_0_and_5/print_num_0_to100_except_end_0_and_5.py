4dead1c3aaba1cca2f36428b7a94196ecf74f216
RodneyGG <rodneyarevalo9963@gmail.com>
2025-03-13 08:04:51 +0800

    fixed spaces

--- print_num_0_to_100_except_end_0_and_5/print_num_0_to100_except_end_0_and_5.py
+++ print_num_0_to_100_except_end_0_and_5/print_num_0_to100_except_end_0_and_5.py
@@ -15,7 +15,7 @@ try_again = True
 
 while try_again:
     #Print numbers from 0 to 100
-    for i in range(0,101):
+    for i in range(0, 101):
         #Check if number end with 0 and 5
         if i % 10 != 0 and i % 5 != 0 and i != 0:
             #print number that not end in 5 and 0
